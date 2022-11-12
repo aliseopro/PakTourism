272dc2bb12180828a7f9b68e77144e3542f0a1bd
Kairavi28 <kairavi.parmar2014@gmail.com>
2022-11-11 20:28:31 -0500

    changes

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -42,9 +42,8 @@
  */
 
 require('dotenv').config();
-//const { MNEMONIC, PROJECT_ID } = process.env;
-
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const { INFURA_API_KEY, MNEMONIC } = process.env;
 
 module.exports = {
   /**
@@ -83,11 +82,13 @@ module.exports = {
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
     // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
+    //   provider: () => new HDWalletProvider(MNEMONIC, 'https://goerli.infura.io/v3/' + INFURA_API_KEY),
     //   network_id: 5,       // Goerli's id
     //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    //   gas: 4465030,
+    //   gasPrice: 10000000000,
+    //   // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //   // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
     //
     // Useful for private networks
